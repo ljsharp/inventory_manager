56f570f0134939fec0451bdf907c6af85d16e35a
Leslie Joe <leslie.kojo.okyere@outlook.com>
2025-03-10 21:38:13 +0000

    added admin routes

---
+++ routes/admin.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\Admin;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', Admin\DashboardController::class)->name('admin.dashboard');
+
+    Route::resources([
+        'warehouses' => Admin\WarehouseController::class,
+    ]);
+});
