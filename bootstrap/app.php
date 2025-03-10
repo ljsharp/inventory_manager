367801bc115de0060970231fb2aac6a234b5be85
Leslie Joe <leslie.kojo.okyere@outlook.com>
2025-03-10 22:12:02 +0000

    add api routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,7 @@ use Illuminate\Support\Facades\Route;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         // web: __DIR__.'/../routes/web.php',
+        // api: __DIR__.'/../routes/api.php',
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
         using: function () {
@@ -18,6 +19,10 @@ return Application::configure(basePath: dirname(__DIR__))
                 ->name('admin.')
                 ->prefix('/admin')
                 ->group(base_path('routes/admin.php'));
+
+            Route::middleware('api')
+                ->prefix('api')
+                ->group(base_path('routes/api.php'));
 
             Route::middleware(['web'])
                 ->group(base_path('routes/web.php'));
