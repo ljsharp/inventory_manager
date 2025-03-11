ce2fa57074142fd4d3e3a6b44fca8a21f79d6930
Leslie Joe <leslie.kojo.okyere@outlook.com>
2025-03-11 11:00:34 +0000

    replace Message component for error message

--- resources/js/pages/Admin/Warehouse/Form.vue
+++ resources/js/pages/Admin/Warehouse/Form.vue
@@ -4,6 +4,7 @@ import { useForm } from '@inertiajs/vue3';
 import Button from 'primevue/button';
 import InputMask from 'primevue/inputmask';
 import InputText from 'primevue/inputtext';
+import Message from 'primevue/message';
 import { onMounted } from 'vue';
 
 const props = defineProps<{
@@ -53,25 +54,25 @@ const submit = () => {
             <div class="flex flex-col items-start gap-1">
                 <label>Name</label>
                 <InputText v-model="warehouseForm.name" class="w-full" />
-                <em v-if="warehouseForm.errors.name" class="text-sm text-red-500">{{ warehouseForm.errors.name }}</em>
+                <Message v-if="warehouseForm.errors.name" severity="error">{{ warehouseForm.errors.name }}</Message>
             </div>
 
             <div class="flex flex-col items-start gap-1">
                 <label>Location</label>
                 <InputText v-model="warehouseForm.location" class="w-full" />
-                <em v-if="warehouseForm.errors.location" class="text-sm text-red-500">{{ warehouseForm.errors.location }}</em>
+                <Message v-if="warehouseForm.errors.location" severity="error">{{ warehouseForm.errors.location }}</Message>
             </div>
 
             <div class="flex flex-col items-start gap-1">
                 <label>Contact Info</label>
                 <InputMask id="phone" v-model="warehouseForm.contact_info" mask="9999999999" placeholder="0540000009" fluid />
-                <em v-if="warehouseForm.errors.contact_info" class="text-sm text-red-500">{{ warehouseForm.errors.contact_info }}</em>
+                <Message v-if="warehouseForm.errors.contact_info" severity="error">{{ warehouseForm.errors.contact_info }}</Message>
             </div>
 
             <div class="flex flex-col items-start gap-1">
                 <label>Capacity</label>
                 <InputText type="number" v-model="warehouseForm.capacity" class="w-full" />
-                <em v-if="warehouseForm.errors.capacity" class="text-sm text-red-500">{{ warehouseForm.errors.capacity }}</em>
+                <Message v-if="warehouseForm.errors.capacity" severity="error">{{ warehouseForm.errors.capacity }}</Message>
             </div>
 
             <Button label="Save" icon="pi pi-save" class="mt-4" type="submit" />
