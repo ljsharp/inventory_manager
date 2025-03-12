b25da715c5231f91fcfec32df8059203730785b7
Leslie Joe <leslie.kojo.okyere@outlook.com>
2025-03-12 16:11:59 +0000

    update the api routes

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Http\Controllers\Admin\StockReportController;
-use App\Http\Controllers\Admin\WarehouseController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,6 +7,8 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+// Route::get('/get-product-warehouses', [StockManagementController::class, 'getProductWarehouses'])
+//     ->name('get.product.warehouses');
 // Route::apiResource('warehouses', WarehouseController::class);
 // Route::get('/get-stock-availability', [StockReportController::class, 'getStockAvailability']);
 // Route::get('/get-warehouse-based-stocks/{warehouse:name}', [StockReportController::class, 'getWarehouseBasedStocks']);
