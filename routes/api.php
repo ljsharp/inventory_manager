1f8244cb53237d9e60f780ead6e7c7a38b2ba4d8
Leslie Joe <leslie.kojo.okyere@outlook.com>
2025-03-12 09:07:01 +0000

    comment out some routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\StockReportController;
 use App\Http\Controllers\Admin\WarehouseController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -8,4 +9,7 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::apiResource('warehouses', WarehouseController::class);
+// Route::apiResource('warehouses', WarehouseController::class);
+// Route::get('/get-stock-availability', [StockReportController::class, 'getStockAvailability']);
+// Route::get('/get-warehouse-based-stocks/{warehouse:name}', [StockReportController::class, 'getWarehouseBasedStocks']);
+// Route::get('/get-stock-transactions/{product}', [StockReportController::class, 'getStockTransactionHistories']);
