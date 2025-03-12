23dae9ec1385af8d62edc0282f358c5631f6e8c6
Leslie Joe <leslie.kojo.okyere@outlook.com>
2025-03-12 13:22:13 +0000

    added stock reports, stock adjustment and stock stransfers routes

--- routes/admin.php
+++ routes/admin.php
@@ -11,4 +11,26 @@ Route::middleware(['auth', 'verified'])->group(function () {
         'categories' => Admin\CategoryController::class,
         'products' => Admin\ProductController::class,
     ]);
+
+    Route::get('/stocks', Admin\StockViewController::class)->name('stocks.view');
+
+    // Stock Reports
+    Route::get('/get-stock-availability', [Admin\StockReportController::class, 'getStockAvailability'])
+        ->name('stock.availability');
+    Route::get('/get-warehouse-based-stocks/{warehouse:name}', [
+        Admin\StockReportController::class,
+        'getWarehouseBasedStocks'
+    ])->name('warehouse.based.stocks');
+    Route::get('/get-stock-transactions/{product}', [
+        Admin\StockReportController::class,
+        'getStockTransactionHistories'
+    ])->name('stock.transactions');
+
+    // Stock Adjustments and Stock Transfers
+    Route::get('/get-product-warehouses', [Admin\StockManagementController::class, 'getProductWarehouses'])
+        ->name('get.product.warehouses');
+    Route::post('/stock-adjustments', [Admin\StockManagementController::class, 'stockAdjustment'])
+        ->name('stock.adjustments');
+    Route::post('/stock-transfers', [Admin\StockManagementController::class, 'stockTransfers'])
+        ->name('stock.transfers');
 });
